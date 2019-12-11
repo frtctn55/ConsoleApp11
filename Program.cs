9aed234d4aaa0ff52959791456e332bffa514f80
Fırat <58728058+frtctn55@users.noreply.github.com>
2019-12-11 11:46:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Random rdn = new Random();
+            int rastgele = rdn.Next(0, 10);
+            while (true)
+            {
+                Console.Write("Sayı Giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi == rastgele)
+                {
+                    break;
+                }
+                i++;
+            }
+            Console.WriteLine(i + ". tahmininizde sayıyı buldunuz.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
